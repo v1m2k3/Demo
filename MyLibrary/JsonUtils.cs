f4da1855efffe6a3a83d05266e813ac9a0756a37
Manoj Vutukuri <Manoj.Vutukuri@mendix.com>
2023-06-21 23:43:42 +0530

    Update .NET to 6

--- MyLibrary/JsonUtils.cs
+++ MyLibrary/JsonUtils.cs
@@ -4,12 +4,12 @@ namespace MyLibrary
 {
     public static class JsonUtils
     {
-        public static string GetJson(Object aObject)
+        public static string? GetJson(Object aObject)
         {
             return aObject is null ? null : Newtonsoft.Json.JsonConvert.SerializeObject(aObject);
         }
 
-        public static T GetObject<T>(string aJson)
+        public static T? GetObject<T>(string aJson)
         {
             return string.IsNullOrEmpty(aJson) ? default : Newtonsoft.Json.JsonConvert.DeserializeObject<T>(aJson);
         }
